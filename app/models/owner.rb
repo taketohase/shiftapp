28fd7b2ab1c194ecd3cfc1111f96fc12d69b555b
unknown <at1581emi@gmail.com>
2021-03-10 02:05:06 +0900

    validation編集

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -7,8 +7,9 @@ class Owner < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :requests, dependent: :destroy
 
-  validates :name, {presence: true}
-  validates :email, {presence:true, uniqueness: true}
+  validates :name, {presence: true, length: {maximum: 12}}
+  validates :email, {presence: true, uniqueness: true,
+                     length: {maximum: 254}}
   validates :userid, {presence: true,
                       uniqueness: true,
                       length: {minimum: 4, maximum: 16}}
--- app/models/owner_worker.rb
+++ app/models/owner_worker.rb
@@ -2,7 +2,7 @@ class OwnerWorker < ApplicationRecord
   belongs_to :owner
   belongs_to :worker
 
-
+  # 同じ内容のowner_workerが既にある場合trueを返す。
   def self.already_linked?(link)
     if OwnerWorker.find_by(owner_id: link.owner_id, worker_id: link.worker_id)
       return true
